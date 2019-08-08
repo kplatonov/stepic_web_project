f7464309795e4792d3277f076a353c4aa3b80659
Konstantin Platonov <k.platonov@corp.mail.ru>
2019-08-08 18:34:24 +0300

    2.1 MVC фреймворки

---
+++ ask/qa/urls.py
@@ -0,0 +1,9 @@
+urlpatterns = patterns('qa.views',                                              
+   url(r'^$', 'test'),                                                              
+   url(r'^login/.*$', 'test', name='login'),                                    
+   url(r'^signup/.*', 'test', name='signup'),                                   
+   url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),                 
+   url(r'^ask/.*', 'test', name='ask'),                                         
+   url(r'^popular/.*', 'test', name='popular'),                                 
+   url(r'^new/.*', 'test', name='new'),                                         
+)
